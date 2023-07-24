85fb4721531a001468a557fa4f0d94abb01bfd45
Holly Grimm <hgrimm@sustia.com>
2023-07-24 20:57:18 +0000

    :sparkles: pull daily historical eth price, calc in CSV

--- azure-function/GetEtherscanInfo.cs
+++ azure-function/GetEtherscanInfo.cs
@@ -16,6 +16,7 @@ namespace Plugins.EtherscanPlugin
 
         private readonly string EndpointURL;
         private readonly string EndpointTxListURL;
+        private readonly string EndpointETHDailyPriceURL;
 
         public GetEtherscanData(IEtherscanSettings etherscanSettings, ILoggerFactory loggerFactory)
         {
@@ -24,12 +25,13 @@ namespace Plugins.EtherscanPlugin
 
             EndpointURL = "https://api.etherscan.io/";
             // pull the last two transactions defined by offset
-            EndpointTxListURL = EndpointURL + "api?module=account&action=txlist&page=1&offset=2&address={address}&apikey=" + etherscanSettings.EtherscanApiKey;
+            EndpointTxListURL = EndpointURL + "api?module=account&action=txlist&page=1&offset=2&sort=desc&address={address}&apikey=" + etherscanSettings.EtherscanApiKey;
+            EndpointETHDailyPriceURL = "https://api.coingecko.com/api/v3/coins/ethereum/history?date={date}&localization=false";
         }
 
         [OpenApiOperation(operationId: "GetTxList", tags: new[] { "ExecuteFunction" }, Description = "Get a list of transactions by wallet address")]
         [OpenApiParameter(name: "walletAddr", Description = "Etherscan Wallet Address", Required = true, In = ParameterLocation.Query)]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The list of transactions by wallet address")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(string), Description = "The list of transactions by wallet address")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "application/json", bodyType: typeof(string), Description = "Returns the error of the input.")]
         [Function("GetTxList")]
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
@@ -58,14 +60,34 @@ namespace Plugins.EtherscanPlugin
                     JObject txListObjResult = new JObject();
                     txListObjResult["result"] = new JArray(txListObj["result"].Select(r => new JObject(
                         new JProperty("blockNumber", r["blockNumber"]),
-                        new JProperty("timeStamp", r["timeStamp"]),
+                        // convert r["timeStamp"] to date
+                        new JProperty("date", DateTimeOffset.FromUnixTimeSeconds(long.Parse(r["timeStamp"].ToString())).DateTime.ToString("dd-MM-yyyy")),
                         new JProperty("transactionIndex", r["transactionIndex"]),
                         new JProperty("from", r["from"]),
                         new JProperty("to", r["to"]),
                         new JProperty("value", r["value"]),
                         new JProperty("gas", r["gas"]),
-                        new JProperty("gasPrice", r["gasPrice"])
+                        new JProperty("gasPrice", r["gasPrice"]),
+                        new JProperty("ethPrice", 1800)
                     )));
+
+                    // for each transaction, get the ETH price for that day using EndpointETHDailyPriceURL
+                    foreach (JObject tx in txListObjResult["result"])
+                    {
+                        string ethDailyPriceURL = EndpointETHDailyPriceURL.Replace("{date}", tx["date"].ToString());
+                        string ethDailyPrice = client.GetStringAsync(ethDailyPriceURL).Result;
+
+                        // parse ethDailyPrice into json object
+                        JObject ethDailyPriceObj = JObject.Parse(ethDailyPrice);
+
+                        // create a new JObject, and add the result from ethDailyPriceObj
+                        JObject ethDailyPriceObjResult = new JObject();
+                        ethDailyPriceObjResult["result"] = new JArray(ethDailyPriceObj["market_data"]["current_price"]["usd"]);
+
+                        // add the ethPrice to the txListObjResult
+                        tx["ethPrice"] = ethDailyPriceObjResult["result"][0];
+                    }
+
                     _logger.LogInformation("txList: " + txListObjResult["result"].ToString());
                     response.WriteString(txListObjResult["result"].ToString());
 
